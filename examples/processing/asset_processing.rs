05af5e7b4fbc84430a58ce847074fca1e1da6b59
jonathandw743 <jonathandw414@gmail.com>
2025-05-02 14:32:23 +0100

    processing example update

--- examples/processing/asset_processing.rs
+++ examples/processing/asset_processing.rs
@@ -28,41 +28,45 @@ fn setup(mut cmd: Commands, server: Res<AssetServer>) {
     cmd.spawn((Camera2d, Transform::default().with_scale(Vec3::splat(0.15))));
 
     cmd.spawn((
-        AseSpriteAnimation {
+        AseAnimation {
             animation: Animation::tag("walk-right"),
             aseprite: server.load("player.aseprite"),
         },
+        Sprite::default(),
         Transform::from_translation(Vec3::new(15., 0., 0.)),
     ));
 
     cmd.spawn((
-        AseSpriteAnimation {
+        AseAnimation {
             animation: Animation::tag("walk-up"),
             aseprite: server.load("player.aseprite"),
         },
+        Sprite::default(),
         Transform::from_translation(Vec3::new(0., 0., 0.)),
     ));
 
     cmd.spawn((
-        AseSpriteAnimation {
+        AseAnimation {
             animation: Animation::tag("walk-down"),
             aseprite: server.load("player.aseprite"),
         },
+        Sprite::default(),
         Transform::from_translation(Vec3::new(-15., 0., 0.)),
     ));
 
     cmd.spawn((
-        AseSpriteAnimation {
+        AseAnimation {
             animation: Animation::default()
                 .with_direction(AnimationDirection::Reverse)
                 .with_repeat(AnimationRepeat::Count(1)),
             aseprite: server.load("player.aseprite"),
         },
+        Sprite::default(),
         Transform::from_translation(Vec3::new(0., -20., 0.)),
     ));
 
     cmd.spawn((
-        AseSpriteAnimation {
+        AseAnimation {
             animation: Animation::tag("walk-right"),
             aseprite: server.load("player.aseprite"),
         },
@@ -74,23 +78,25 @@ fn setup(mut cmd: Commands, server: Res<AssetServer>) {
     ));
 
     cmd.spawn((
-        AseSpriteAnimation {
+        AseAnimation {
             animation: Animation::default().with_tag("squash"),
             aseprite: server.load("ball.aseprite"),
         },
+        Sprite::default(),
         Transform::from_translation(Vec3::new(0., 20., 0.)),
     ));
 
     cmd.spawn((
-        AseSpriteSlice {
+        AseSlice {
             name: "ghost_red".into(),
             aseprite: server.load("ghost_slices.aseprite"),
         },
+        Sprite::default(),
         Transform::from_translation(Vec3::new(50., 0., 0.)),
     ));
 
     cmd.spawn((
-        AseSpriteSlice {
+        AseSlice {
             name: "ghost_blue".into(),
             aseprite: server.load("ghost_slices.aseprite"),
         },
@@ -105,7 +111,7 @@ fn setup(mut cmd: Commands, server: Res<AssetServer>) {
 fn events(mut events: EventReader<AnimationEvents>, mut cmd: Commands) {
     for event in events.read() {
         match event {
-            AnimationEvents::Finished(entity) => cmd.entity(*entity).despawn_recursive(),
+            AnimationEvents::Finished(entity) => cmd.entity(*entity).despawn(),
             AnimationEvents::LoopCycleFinished(_entity) => (),
         };
     }
